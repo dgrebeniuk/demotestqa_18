3144f32ba7884513d3f2e3e1e2625d28e435b344
Dmitrii Grebeniuk <dmtrgrbnk@gmail.com>
2023-02-22 20:26:34 +0300

    update firstTest

--- src/test/java/JunitTest.java
+++ src/test/java/JunitTest.java
@@ -1,9 +1,37 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class JunitTest {
 
+   @BeforeAll
+   static void beforeAll() {
+      System.out.println("Here is my beforeAll()");
+   }
+
+   @BeforeEach
+   void setUp() {
+      System.out.println("    Here is my setUp()");
+   }
+
+   @AfterAll
+   static void afterAll() {
+      System.out.println("                Here is my afterAll()");
+   }
+
+   @AfterEach
+   void tearDown() {
+      System.out.println("                   Here is my tearDown()");
+   }
+
    @Test
    void firstTest(){
-      System.out.println("Here is my firstTest()");
+      System.out.println("          Here is my firstTest()");
    }
+
+   @Test
+   void secondTest(){
+      System.out.println("          Here is my secondTest()");
+   }
+
+
+
 }
