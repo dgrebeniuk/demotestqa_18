413deac86f3e5b45115e79f3c240d8ae864aa24e
Dmitrii Grebeniuk <dmtrgrbnk@gmail.com>
2023-04-09 20:39:54 +0300

    update locators

--- src/test/java/TestRegistration.java
+++ src/test/java/TestRegistration.java
@@ -1,11 +1,12 @@
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byName;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.files.DownloadActions.click;
 import static java.awt.SystemColor.control;
 
@@ -14,6 +15,7 @@ public class TestRegistration {
    @BeforeAll
    static void beforeAll() {
       Configuration.browserSize = "1680x1050";
+
    }
 
    @Test
@@ -21,30 +23,46 @@ public class TestRegistration {
 
       open("https://demoqa.com/automation-practice-form");
 
+      Selenide.executeJavaScript("$('#fixedban').remove()");
+      Selenide.executeJavaScript("$('footer').remove()");
+
       $("#firstName").setValue("Maxim"); //Name
       $("#lastName").setValue("Evdokimov"); //Last name
       $("#userEmail").setValue("evdokimov@gmail.com"); //Email
       $(".custom-radio:nth-child(1) > .custom-control-label").click(); //Gender
       $("#userNumber").setValue("9313859692"); //Mobile
-      $("#dateOfBirthInput").setValue("05 Mar 2000"); //Date of Birth
-      $("[id=subjectsContainer]").click(); //Subjects
-      $("#react-select-2-option-0").click();
-      $(".custom-checkbox:nth-child(1) > .custom-control-label").click(); //Hobbies
-      $("#uploadPicture").sendKeys("/Users/dmitrijgrebenyuk/Documents/Difrent format files/test.png"); //Picture
-      $("#currentAddress").setValue("Tbilisi"); //Current Address
-      //$("#dateOfBirthInput").selectRadio("Hindi"); //State
-     // $("#dateOfBirthInput").selectRadio("Hindi"); //City
 
+      $("#dateOfBirthInput").click(); //Date of Birth
+      $(".react-datepicker__week > .react-datepicker__day--030").click(); //Date of Birth
+
+      $("#subjectsInput").sendKeys("Maths");
+      $("#subjectsWrapper .subjects-auto-complete__option").click();
+
+      $("#hobbiesWrapper").$(byText("Sports")).click(); //Hobbies
+
+      $("#uploadPicture").sendKeys("/Users/dmitrijgrebenyuk/Documents/Difrent format files/test.png"); //Picture
+
+      $("#currentAddress").setValue("Tbilisi"); //Current Address
+
+      $("#state").click(); //State
+      $("#state").$(byText("NCR")).click();
+
+      $("#city").click(); //City
+      $("#city").$(byText("Delhi")).click();
 
       $("#submit").click(); //Click on the button
+      
 
-
-
-      $("#example-modal-sizes-title-lg").shouldHave(text("Maxim"), //Check up test
+      $(".table-responsive").shouldHave(text("Maxim"), //Check up test
               text("Evdokimov"),
               text("evdokimov@gmail.com"),
               text("Male"),
-              text("9313859692"));
+              text("9313859692"),
+              text("30 March,2023"),
+              text("Maths"),
+              text("test.png"),
+              text("Tbilisi"),
+              text("NCR Delhi"));
 
 
 
