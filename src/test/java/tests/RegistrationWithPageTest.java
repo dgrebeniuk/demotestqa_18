bdc88b588f29ee53a6feb41ffab6fd0c845d37c8
Dmitrii Grebeniuk <dmtrgrbnk@gmail.com>
2023-05-05 12:17:35 +0300

    add random dates

--- src/test/java/tests/RegistrationWithPageTest.java
+++ src/test/java/tests/RegistrationWithPageTest.java
@@ -9,31 +9,46 @@ public class RegistrationWithPageTest extends TestBase {
    @Test
    void registrationFrom() {
 
+      String firstName = faker.name().firstName(),
+      String lastName = faker.name().lastName(),
+      String userEmail = faker.internet().emailAddress(),
+      String gender = ,
+      String userPhone = "+7" + faker.phoneNumber().subscriberNumber(10),
+      String dayOfBirth = ,
+      String monthOfBirth = ,
+      String yearOfBirth = ,
+      String subject = ,
+      String hobbies = ,
+      String pictureName = "img/test.png",
+      String address = faker.address().country(),
+      String state = "NCR",
+      String city = faker.address().city();
+
       registrationPage.openPage()
-              .setFirstName("Maxim")
-              .setLastName("Evdokimov")
-              .setUserEmail("evdokimov@gmail.com")
-              .setGender("Male")
-              .setPhone("9313859692")
-              .setDateBirth("30", "March", "2023")
+              .setFirstName(firstName)
+              .setLastName(lastName)
+              .setUserEmail(userEmail)
+              .setGender(gender)
+              .setPhone(userPhone)
+              .setDateBirth(dayOfBirth, monthOfBirth, yearOfBirth)
               .setSubject("Maths")
               .setHobbies("Sports")
-              .uploadPicture("img/test.png")
-              .setAddress("Tbilisi")
-              .setState("NCR")
+              .uploadPicture(pictureName)
+              .setAddress(address)
+              .setState(state)
               .setCity("Delhi")
               .setSubmitButton();
 
       registrationPage
-              .verifyResult("Student Name", "Maxim Evdokimov")
-              .verifyResult("Student Email", "evdokimov@gmail.com")
+              .verifyResult("Student Name", firstName + lastName)
+              .verifyResult("Student Email", userEmail)
               .verifyResult("Gender", "Male")
-              .verifyResult("Mobile", "9313859692")
+              .verifyResult("Mobile", userPhone)
               .verifyResult("Date of Birth", "30 March,2023")
               .verifyResult("Subjects", "Maths")
               .verifyResult("Hobbies", "Sports")
               .verifyResult("Picture", "test.png")
-              .verifyResult("Address", "Tbilisi")
-              .verifyResult("State and City", "NCR Delhi");
+              .verifyResult("Address", address)
+              .verifyResult("State and City", state + city);
    }
 }
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,15 +2,16 @@ package tests;
 
 import Pages.RegistrationPage;
 import com.codeborne.selenide.Configuration;
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
 
    RegistrationPage registrationPage = new RegistrationPage();
+   Faker faker = new Faker();
 
    @BeforeAll
    static void beforeAll() {
       Configuration.browserSize = "1680x1050";
-
    }
 }
